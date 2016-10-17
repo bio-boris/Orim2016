bd4e8cb5f88ce119c51516f42026bc4a9747b7fa
bio-boris <bio-boris@users.noreply.github.com>
2016-10-17 18:20:52 -0500

    Update and rename README.md to download.sh

---
+++ data/download.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+export USER_NAME=user
+export USER_PASSWORD=password
+
+curl 'https://signon.jgi.doe.gov/signon/create' --data-urlencode 'login=$USER_NAME' --data-urlencode 'password=$USER_PASSWORD' -c cookies > result
+
+curl 'http://genome.jgi.doe.gov/ext-api/downloads/get-directory?organism=PhytozomeV11' -b cookies > phytozome11.xml
+
+./getData.py phytozome11.xml
+
+
