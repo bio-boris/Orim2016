514bfb3402f719ae1fee4c1efe7f5de0e6b4cf19
bio-boris <bio-boris@users.noreply.github.com>
2016-10-17 19:00:35 -0500

    Update download.sh

--- data/download.sh
+++ data/download.sh
@@ -3,12 +3,10 @@
 export USER_NAME=user
 export USER_PASSWORD=password
 
-curl 'https://signon.jgi.doe.gov/signon/create' --data-urlencode 'login=$USER_NAME' --data-urlencode 'password=$USER_PASSWORD' -c cookies > result
-
+curl 'https://signon.jgi.doe.gov/signon/create' --data-urlencode login=$USER_NAME --data-urlencode password=$USER_PASSWORD -c cookies > result
 curl 'http://genome.jgi.doe.gov/ext-api/downloads/get-directory?organism=PhytozomeV11' -b cookies > phytozome11.xml
 
-./getData.py phytozome11.xml > downloadList
-
+./getPhytozomeData.py phytozome11.xml > downloadList
 sh downloadList
 
 
