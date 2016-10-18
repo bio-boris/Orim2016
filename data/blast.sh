5b5d7967bab3aba0920dc29f5fc02a91872d1876
bio-boris <bio-boris@users.noreply.github.com>
2016-10-18 13:00:52 -0500

    Update blast.sh

--- data/blast.sh
+++ data/blast.sh
@@ -3,6 +3,6 @@
 source config.sh
 mkdir blast
 
-time ./formatdb.py $LIST_OF_PROTEIN_FILES > formatdb_time
+time ./formatdb.py $PROTEIN_FILES > formatdb_time
 
 time ./blastall_phytozome.py -i $I -b $B -topHits $TOPHITS -processors $PROCESSORS -eval $EVAL  > blastTime
