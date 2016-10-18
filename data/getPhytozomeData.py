bd6aea4acb92514b9ff6d5925b013a87817029c1
bio-boris <bio-boris@users.noreply.github.com>
2016-10-17 19:50:21 -0500

    Update getPhytozomeData.py

--- data/getPhytozomeData.py
+++ data/getPhytozomeData.py
@@ -33,4 +33,4 @@ for url in urls:
 for download in downloads:
         filename = os.path.basename(download)
         print "echo " + filename
-        print "curl '" + download + "' -b cookies > " + filename
+        print "wget '" + download + "' --load-cookies cookies > " + filename
